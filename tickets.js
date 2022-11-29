037b0330d674a374ef9b1d9c447891756c7c93b0
MicaelaT06 <micaelasoliz@hotmail.com>
2022-11-29 15:02:40 -0300

    cambios

--- tickets.js
+++ tickets.js
@@ -1,9 +1,17 @@
 // Variables**************************
+const studentDiscount = document.querySelector(".student")
+const traineeDiscount = document.querySelector(".trainee")
+const juniorDiscount= document.querySelector(".junior")
+
+console.log(studentDiscount)
+console.log(traineeDiscount)
+console.log(juniorDiscount)
+
 const form = document.forms.form;
 console.log(form)
 
-const quantity = document.getElementById("cantidadTickets");
-console.log(cantidadTickets);
+const amount = document.getElementById("amountOfTickets");
+console.log(amountOfTickets);
 
 const category = document.getElementById("selectionCategory");
 console.log(selectionCategory);
@@ -11,23 +19,16 @@ console.log(selectionCategory);
 const totalBuy = document.getElementById("totalBuy");
 console.log(totalBuy);
 
-const btnDeleted = document.getElementById("btnBorrar");
-console.log(btnBorrar);
+const deletedAll = document.getElementById("deleteButton");
+console.log(deleteButton);
 
-const resumen = document.getElementById("btnResumen");
-console.log(btnResumen);
-
-//Eventos
-let botonBorrar = document.getElementById('btnBorrar');
-botonBorrar.addEventListener("click", borrarForm);
-
-let botonResumen = document.getElementById('btnResumen');
-botonResumen.addEventListener("click", ticketsTotal);
+const summary = document.getElementById("summaryButton");
+console.log(summaryButton);
 
 
 // Validar Formulario*********************
 
-function validarForm() {
+function validateForm() {
     let inputForm = []
     form.firstName.value === ''
         ? form.firstName.classList.add('border-danger')   
@@ -38,57 +39,62 @@ function validarForm() {
     form.email.value === ''
         ? form.email.classList.add('border-danger')   
         : form.email.classList.remove('border-danger')
-    form.cantidadTickets.value === ''
-        ? form.cantidadTickets.classList.add('border-danger')   
-        : form.cantidadTickets.classList.remove('border-danger')
+    form.amountOfTickets.value === ''
+        ? form.amountOfTickets.classList.add('border-danger')   
+        : form.amountOfTickets.classList.remove('border-danger')
 }
-
 
 
 //Funcion Resumen*************************
 
 
-function ticketsTotal(e) {
+function totalTickets(e) {
     e.preventDefault();
-    let cantidadTickets = document.getElementById('cantidadTickets');
-    if (Number(cantidadTickets.value)) { 
+    let amountOfTickets = document.getElementById('amountOfTickets');
+    if (Number(amountOfTickets.value)) { 
         let selectionCategory = document.getElementById('selectionCategory');
         let totalBuy;
         switch (selectionCategory.value){
-            case "valueEstudiante": {
-                totalBuy = 200 * cantidadTickets.value * 0.2;
+            case "valueStudent": {
+                totalBuy = 200 * amountOfTickets.value * 0.2;
                 break;
             }
             case "valueTrainee": {
-                totalBuy = 200 * cantidadTickets.value * 0.5;
+                totalBuy = 200 * amountOfTickets.value * 0.5;
                 break;
             }
             case "valueJunior": {
-                totalBuy = 200 * cantidadTickets.value * 0.85;
+                totalBuy = 200 * amountOfTickets.value * 0.85;
                 break;
             }
         }
         document.getElementById('totalBuy').textContent = `Total a Pagar: $${totalBuy}`;
     }  
-    validarForm()
+    validateForm()
 }
+
+let summaryEvent = document.getElementById('summaryButton');
+summaryEvent.addEventListener("click", totalTickets);
 
 
 //Funcion boton BORRAR**********************
 
-function borrarForm(e) {
+function deleteForm(e) {
     e.preventDefault()
     form.firstName.value = ''
     form.lastName.value = ''
     form.email.value = ''
-    form.cantidadTickets.value = ''
+    form.amountOfTickets.value = ''
     form.selectionCategory.value = ''
 }
 
-function borrarForm() {
+function deleteForm() {
     form.firstName.classList.replace('border-danger', 'default');
     form.lastName.classList.replace('border-danger', 'default');
     form.email.classList.replace('border-danger', 'default');
-    form.cantidadTickets.classList.replace('border-danger', 'default');
+    form.amountOfTickets.classList.replace('border-danger', 'default');
     document.getElementById('totalBuy').textContent = `Total a Pagar: $${''}`;
 }   
+
+let buttonDeleted = document.getElementById('deleteButton');
+buttonDeleted.addEventListener("click", deleteForm);
